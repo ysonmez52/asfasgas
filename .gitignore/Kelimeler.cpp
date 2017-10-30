fc03f409903af48ecb7d6fb373017fa466b98f4f
ysonmez52 <33210762+ysonmez52@users.noreply.github.com>
2017-10-30 05:57:10 +0300

    Add files via upload

---
+++ .gitignore/Kelimeler.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+main() {
+    		int x=0;
+	char a[20]="";
+	printf("Bir kelime giriniz=");
+	scanf("%s",kelime);
+	printf("Kac kere yazilsin ?");
+	scanf("%d", &x);
+	
+	for(int i=0; i<x; i++)
+	printf("%s \n",a);
+	
+	
+	getch();
+	
+}
